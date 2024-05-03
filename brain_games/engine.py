b88d0d72cf056abd160e376118faf1c9b6d8576e
LightFalse <Danoneover9999@gmail.com>
2024-05-03 23:25:09 +0300

    engine.py

---
+++ brain_games/engine.py
@@ -0,0 +1,27 @@
+import prompt
+from brain_games.cli import welcome_user
+
+
+def game(name_game, rules):
+    rules = rules()
+    name = welcome_user()
+    counter = 0
+
+    print(rules)
+    while counter < 3:
+        question, ans = name_game()
+        print("Question:", question)
+        us_ans = prompt.string("Your answer:")
+        if str(ans) == us_ans:
+            counter += 1
+            print("Correct!")
+        else:
+            print(f" '{us_ans}' is wrong answer ;(. Correct answer was '{ans}'.")
+            print(f"Let's try again, {name}! ")
+            return counter
+    if counter == 3:
+        print(f"Congratulations, {name}!")
+
+
+if __name__ == "__main__":
+    game()
